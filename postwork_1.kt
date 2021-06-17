1a35b51168c5ae6ed2b797b66c83e85c18c1872f
Hisseon <79939665+Hisseon@users.noreply.github.com>
2021-06-16 21:10:49 -0500

    Update postwork_1.kt

--- postwork_1.kt
+++ postwork_1.kt
@@ -1,1 +1,6 @@
-
+fun main(){
+    println("Ingresa el nombre del profesor: ")
+    var profesor1 = readLine()
+    println("Ingresa la calificacion del profesor del 1 al 5: ")
+    var calif1 = readLine()
+}
