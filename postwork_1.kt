cbcd95fd14902ff96a3146b2e1c9a00916e71d88
Hisseon <79939665+Hisseon@users.noreply.github.com>
2021-06-16 21:12:27 -0500

    Update postwork_1.kt

--- postwork_1.kt
+++ postwork_1.kt
@@ -3,4 +3,6 @@ fun main(){
     var profesor1 = readLine()
     println("Ingresa la calificacion del profesor del 1 al 5: ")
     var calif1 = readLine()
+    println("La calificacion del profesor $profesor1 segun la comunidad es de: $calif1 estrellas")
+    
 }
