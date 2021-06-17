e8e1b6f36f5e04459bcc71d387060f88c06cf0de
Esiel Arizmendi <esiel_kar@hotmail.com>
2021-06-16 21:44:01 -0500

    Update postwork_1.kt

--- postwork_1.kt
+++ postwork_1.kt
@@ -1,8 +1,29 @@
 fun main(){
-    println("Ingresa el nombre del profesor: ")
-    var profesor1 = readLine()
-    println("Ingresa la calificacion del profesor del 1 al 5: ")
-    var calif1 = readLine()
-    println("La calificacion del profesor $profesor1 segun la comunidad es de: $calif1 estrellas")
-    
+    var opc = 1;
+
+    do{
+        println("Ingresa el nombre del profesor: ")
+        var profeName = readLine()!!
+
+        println("Ingresa el apellido paterno del profesor: ")
+        var profeApPat = readLine()!!
+
+        println("Ingresa el apellido materno del profesor: ")
+        var profeApMat = readLine()!!
+
+        var calif1 : Int
+        do {
+            println("Ingresa la calificacion del profesor $profeName $profeApPat $profeApMat del 1 al 5: ")
+            calif1 = readLine()!!.toInt()
+            if (calif1 !in 1..5)
+                println("La calificación debe ser en el rango de 1-5")
+        }while(calif1 !in 1..5)
+
+        println("La calificacion del profesor $profeName $profeApPat $profeApMat  segun la comunidad es de: $calif1 estrellas")
+        println("Gustas agregar a otro profesor?")
+        println("1. Si")
+        println("2. No")
+        opc = readLine()!!.toInt()
+    }while(opc != 2)
+
 }
