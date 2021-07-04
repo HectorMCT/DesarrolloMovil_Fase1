624b9b278498b381d399322f089acbaf01864ea9
Esiel Arizmendi <esiel_kar@hotmail.com>
2021-07-04 16:54:01 -0500

    Update Review.kt
    Se añadió la fecha en la que se crea la review, utilizando así las librerías java.text y java.util con las clases SimpleDateFormat y Date, respectivamente.

--- Calificame/src/main/kotlin/model/Review.kt
+++ Calificame/src/main/kotlin/model/Review.kt
@@ -1,15 +1,20 @@
 package model
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 /**
  * Representa una review de la aplicación Califícame!
  * @constructor Crea una nueva reseña dado un [User], comment y satisfaction
- * @property User Usiario que hace la review
+ * @property user Usuario que hace la review
+ * @property date fecha en la que se hace la review (Por defecto es la actual cuando se crea)
  * @property comment Comentario que hace el usuario
  * @property satisfaction Grado de satisfaction del usuario
  */
-data class Review (val user : User, val comment : String, val satisfaction : Double){
+data class Review (val user : User, val comment : String, val satisfaction : Double, private val date : Date = Date()){
     override fun toString(): String {
 return """User: ${user.username}
+Date: ${SimpleDateFormat("dd.MM.yyyy hh:mm:ss").format(date)}
 Satisfaction: $satisfaction
 Comment: $comment 
 """.trimIndent()
